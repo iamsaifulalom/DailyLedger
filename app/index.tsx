01e07cd8836a3304c444482254253cee7c76fa10
Saiful Alom <saif.zsdb2gmail.com>
2025-09-17 17:01:14 +0600

    icon changed

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,5 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { Redirect } from "expo-router";
 
 export default function Index() {
-  return (
-    <View>
-      <Text>Index</Text>
-    </View>
-  )
+  return <Redirect href="/(tabs)/dashboard" />;
 }
